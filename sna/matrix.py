ae1d3ae07c8df10de993271ec8c886d92b1a52ad
aguestuser <aguestuser@riseup.net>
2017-10-20 21:20:12 -0400

    calculate adjacency matrix w/ linear algebra -> TERSE CITY!!!

--- sna/matrix.py
+++ sna/matrix.py
@@ -1,44 +1,22 @@
 import numpy as np
 
 
-def parse_adjacency_matrices(incidence_matrix):
-    return {
-        'row': row_adjacencies(incidence_matrix),
-        'col': col_adjacencies(incidence_matrix)
-    }
+def row_adjacencies(inc_matrix):
+    return adjacencies(inc_matrix)
 
 
-def row_adjacencies(incidence_matrix):
-    row_count = len(incidence_matrix)
-    acc = np.zeros((row_count, row_count), dtype=int)
-    for i in range(row_count):
-        for j in range(row_count):
-            if i != j:  # else leave it as 0
-                acc[i][j] = count_common_values(incidence_matrix[i],
-                                                incidence_matrix[j])
-    return acc
+def col_adjacencies(inc_matrix):
+    return adjacencies(inc_matrix.T)
 
 
-def col_adjacencies(incidence_matrix):
-    col_count = len(incidence_matrix[0])
-    acc = np.zeros((col_count, col_count), dtype=int)
-    for i in range(col_count):
-        for j in range(col_count):
-            if i != j:
-                acc[i][j] = count_common_values(
-                    column_from_idx(i, incidence_matrix),
-                    column_from_idx(j, incidence_matrix)
-                )
-    return acc
-
-
-# helpers
-
-
-def column_from_idx(idx, matrix):
-    return [row[idx] for row in matrix]
-
-
-def count_common_values(row1, row2):
-    # TODO: use bit-wise opeators?
-    return sum([1 for x, y in zip(row1, row2) if x + y == 2])
+def adjacencies(A_inc):
+    """
+    taking dot product of inc. matrix and its transpose yields adj. matrix
+    b/c: multiplying matrix w/ transpose:
+        === summing product of ith element in each row
+            w/ ith element of every other row
+        === counting common values btw/ each row and every other row
+    subtracting diagonal removes cycles (ie: product of row and itself)
+    """
+    product = np.dot(A_inc, A_inc.T)
+    return product - np.diag(np.diag(product))
--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -22,32 +22,9 @@ col_adjacency_matrix = np.array([
     [1, 1, 2, 0, 0]])
 
 
-def test_parse_adjacency_matrices():
-    result = m.parse_adjacency_matrices(inc_matrix)
-    expected = {
-        'row': row_adjacency_matrix,
-        'col': col_adjacency_matrix
-    }
-    for k, v in result.iteritems():
-        assert (v == expected[k]).all()
-
-
 def test_row_adjacencies():
     assert (m.row_adjacencies(inc_matrix) == row_adjacency_matrix).all()
 
 
 def test_col_adjacencies():
     assert (m.col_adjacencies(inc_matrix) == col_adjacency_matrix).all()
-
-
-def test_column_from_idx():
-    matrix = [[1, 2, 3],
-              [1, 2, 3],
-              [1, 2, 3]]
-    assert m.column_from_idx(1, matrix) == [2, 2, 2]
-
-
-def test_count_common_values():
-    r1 = [0, 1, 0]
-    r2 = [0, 1, 1]
-    assert m.count_common_values(r1, r2) == 1
